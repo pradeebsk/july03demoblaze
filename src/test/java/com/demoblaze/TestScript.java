bbde3e16e177a7cbf8207ee27e554def22cd406c
pradeebsk <pradeepkumarsvga@gmail.com>
2024-07-03 18:25:37 +0530

    devop

--- src/test/java/com/demoblaze/TestScript.java
+++ src/test/java/com/demoblaze/TestScript.java
@@ -3,7 +3,8 @@ package com.demoblaze;
 public class TestScript extends BaseDemo{
 	public static void main(String[] args) {
 		
-		
+		System.out.println("merge conflict arrised ");
+		System.out.println("rectify it ");
 		
 	}
 	
